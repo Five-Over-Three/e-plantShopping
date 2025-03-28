16d143cd58b264aefdb6d8e534625430e33d4f85
FiveOverThree <fiveoverthreetv@gmail.com>
2025-03-28 10:52:51 +0000

    Added code to bring to starting point for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css'
+import ProductList from './ProductList';
+import AboutUs from './AboutUs';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [showProductList, setShowProductList] = useState(false);
+
+    const handleGetStartedClick = () => {
+        setShowProductList(true);
+    };
+
+    const handleHomeClick = () => {
+        setShowProductList(false);
+    };
+
+    return (
+        <>
+            <div className="app-container">
+                <div className="landing-page">
+                    <div className="background-image"></div>
+                    <div className="content">
+                        <div className="landing_content">
+                            <h1>Welcome to Paradise Nursery</h1>
+                            <div className="divider"></div>
+                            <p>Where Green Meets Serenity</p>
+                            <button className="get-started-button" onClick={handleGetStartedClick}>Get Started</button>
+                        </div>
+                        <div className="aboutus_container">
+                            <AboutUs />
+                        </div>
+                    </div>
+                </div>
+                <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+                    <ProductList onHomeClick={handleHomeClick} />
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
